f9680ed42d31da07c79932c3040f0ffd514a50fc
Ryan <racollette@gmail.com>
2021-11-13 20:16:11 -0700

    chore: rename variables

--- subgraphs/exchange/mappings/core.ts
+++ subgraphs/exchange/mappings/core.ts
@@ -3,7 +3,7 @@ import { BigInt, BigDecimal, store } from "@graphprotocol/graph-ts";
 import {
   Pair,
   Token,
-  PancakeFactory,
+  GlideFactory,
   Transaction,
   Mint as MintEvent,
   Burn as BurnEvent,
@@ -11,8 +11,8 @@ import {
   Bundle,
 } from "../generated/schema";
 import { Mint, Burn, Swap, Transfer, Sync } from "../generated/templates/Pair/Pair";
-import { updatePairDayData, updateTokenDayData, updatePancakeDayData, updatePairHourData } from "./dayUpdates";
-import { getBnbPriceInUSD, findBnbPerToken, getTrackedVolumeUSD, getTrackedLiquidityUSD } from "./pricing";
+import { updatePairDayData, updateTokenDayData, updateGlideDayData, updatePairHourData } from "./dayUpdates";
+import { getElaPriceInUSD, findElaPerToken, getTrackedVolumeUSD, getTrackedLiquidityUSD } from "./pricing";
 import { convertTokenToDecimal, ADDRESS_ZERO, FACTORY_ADDRESS, ONE_BI, ZERO_BD, BI_18 } from "./utils";
 import { log } from '@graphprotocol/graph-ts'
 
@@ -75,7 +75,7 @@ export function handleTransfer(event: Transfer): void {
     }
   }
 
-  // case where direct send first on BNB withdrawals
+  // case where direct send first on ELA withdrawals
   if (event.params.to.toHex() == pair.id) {
     let burns = transaction.burns;
     let burn = new BurnEvent(
@@ -176,10 +176,10 @@ export function handleSync(event: Sync): void {
   let pair = Pair.load(event.address.toHex());
   let token0 = Token.load(pair.token0);
   let token1 = Token.load(pair.token1);
-  let pancake = PancakeFactory.load(FACTORY_ADDRESS);
+  let glide = GlideFactory.load(FACTORY_ADDRESS);
 
   // reset factory liquidity by subtracting only tracked liquidity
-  pancake.totalLiquidityBNB = pancake.totalLiquidityBNB.minus(pair.trackedReserveBNB as BigDecimal);
+  glide.totalLiquidityELA = glide.totalLiquidityELA.minus(pair.trackedReserveELA as BigDecimal);
 
   // reset token total liquidity amounts
   token0.totalLiquidity = token0.totalLiquidity.minus(pair.reserve0);
@@ -194,43 +194,43 @@ export function handleSync(event: Sync): void {
   else pair.token1Price = ZERO_BD;
 
   let bundle = Bundle.load("1");
-  bundle.bnbPrice = getBnbPriceInUSD();
+  bundle.elaPrice = getElaPriceInUSD();
   bundle.save();
 
-  let t0DerivedBNB = findBnbPerToken(token0 as Token);
-  token0.derivedBNB = t0DerivedBNB;
-  token0.derivedUSD = t0DerivedBNB.times(bundle.bnbPrice);
+  let t0DerivedELA = findElaPerToken(token0 as Token);
+  token0.derivedELA = t0DerivedELA;
+  token0.derivedUSD = t0DerivedELA.times(bundle.elaPrice);
   token0.save();
 
-  let t1DerivedBNB = findBnbPerToken(token1 as Token);
-  token1.derivedBNB = t1DerivedBNB;
-  token1.derivedUSD = t1DerivedBNB.times(bundle.bnbPrice);
+  let t1DerivedELA = findElaPerToken(token1 as Token);
+  token1.derivedELA = t1DerivedELA;
+  token1.derivedUSD = t1DerivedELA.times(bundle.elaPrice);
   token1.save();
 
   // get tracked liquidity - will be 0 if neither is in whitelist
-  let trackedLiquidityBNB: BigDecimal;
-  if (bundle.bnbPrice.notEqual(ZERO_BD)) {
-    trackedLiquidityBNB = getTrackedLiquidityUSD(
+  let trackedLiquidityELA: BigDecimal;
+  if (bundle.elaPrice.notEqual(ZERO_BD)) {
+    trackedLiquidityELA = getTrackedLiquidityUSD(
       bundle as Bundle,
       pair.reserve0,
       token0 as Token,
       pair.reserve1,
       token1 as Token
-    ).div(bundle.bnbPrice);
+    ).div(bundle.elaPrice);
   } else {
-    trackedLiquidityBNB = ZERO_BD;
+    trackedLiquidityELA = ZERO_BD;
   }
 
   // use derived amounts within pair
-  pair.trackedReserveBNB = trackedLiquidityBNB;
-  pair.reserveBNB = pair.reserve0
-    .times(token0.derivedBNB as BigDecimal)
-    .plus(pair.reserve1.times(token1.derivedBNB as BigDecimal));
-  pair.reserveUSD = pair.reserveBNB.times(bundle.bnbPrice);
+  pair.trackedReserveELA = trackedLiquidityELA;
+  pair.reserveELA = pair.reserve0
+    .times(token0.derivedELA as BigDecimal)
+    .plus(pair.reserve1.times(token1.derivedELA as BigDecimal));
+  pair.reserveUSD = pair.reserveELA.times(bundle.elaPrice);
 
   // use tracked amounts globally
-  pancake.totalLiquidityBNB = pancake.totalLiquidityBNB.plus(trackedLiquidityBNB);
-  pancake.totalLiquidityUSD = pancake.totalLiquidityBNB.times(bundle.bnbPrice);
+  glide.totalLiquidityELA = glide.totalLiquidityELA.plus(trackedLiquidityELA);
+  glide.totalLiquidityUSD = glide.totalLiquidityELA.times(bundle.elaPrice);
 
   // now correctly set liquidity amounts for each token
   token0.totalLiquidity = token0.totalLiquidity.plus(pair.reserve0);
@@ -238,7 +238,7 @@ export function handleSync(event: Sync): void {
 
   // save entities
   pair.save();
-  pancake.save();
+  glide.save();
   token0.save();
   token1.save();
 }
@@ -249,7 +249,7 @@ export function handleMint(event: Mint): void {
   let mint = MintEvent.load(mints[mints.length - 1]);
 
   let pair = Pair.load(event.address.toHex());
-  let pancake = PancakeFactory.load(FACTORY_ADDRESS);
+  let glide = GlideFactory.load(FACTORY_ADDRESS);
 
   let token0 = Token.load(pair.token0);
   let token1 = Token.load(pair.token1);
@@ -262,22 +262,22 @@ export function handleMint(event: Mint): void {
   token0.totalTransactions = token0.totalTransactions.plus(ONE_BI);
   token1.totalTransactions = token1.totalTransactions.plus(ONE_BI);
 
-  // get new amounts of USD and BNB for tracking
+  // get new amounts of USD and ELA for tracking
   let bundle = Bundle.load("1");
-  let amountTotalUSD = token1.derivedBNB
+  let amountTotalUSD = token1.derivedELA
     .times(token1Amount)
-    .plus(token0.derivedBNB.times(token0Amount))
-    .times(bundle.bnbPrice);
+    .plus(token0.derivedELA.times(token0Amount))
+    .times(bundle.elaPrice);
 
   // update txn counts
   pair.totalTransactions = pair.totalTransactions.plus(ONE_BI);
-  pancake.totalTransactions = pancake.totalTransactions.plus(ONE_BI);
+  glide.totalTransactions = glide.totalTransactions.plus(ONE_BI);
 
   // save entities
   token0.save();
   token1.save();
   pair.save();
-  pancake.save();
+  glide.save();
 
   mint.sender = event.params.sender;
   mint.amount0 = token0Amount as BigDecimal;
@@ -288,7 +288,7 @@ export function handleMint(event: Mint): void {
 
   updatePairDayData(event);
   updatePairHourData(event);
-  updatePancakeDayData(event);
+  updateGlideDayData(event);
   updateTokenDayData(token0 as Token, event);
   updateTokenDayData(token1 as Token, event);
 }
@@ -303,7 +303,7 @@ export function handleBurn(event: Burn): void {
   let burn = BurnEvent.load(burns[burns.length - 1]);
 
   let pair = Pair.load(event.address.toHex());
-  let pancake = PancakeFactory.load(FACTORY_ADDRESS);
+  let glide = GlideFactory.load(FACTORY_ADDRESS);
 
   //update token info
   let token0 = Token.load(pair.token0);
@@ -315,22 +315,22 @@ export function handleBurn(event: Burn): void {
   token0.totalTransactions = token0.totalTransactions.plus(ONE_BI);
   token1.totalTransactions = token1.totalTransactions.plus(ONE_BI);
 
-  // get new amounts of USD and BNB for tracking
+  // get new amounts of USD and ELA for tracking
   let bundle = Bundle.load("1");
-  let amountTotalUSD = token1.derivedBNB
+  let amountTotalUSD = token1.derivedELA
     .times(token1Amount)
-    .plus(token0.derivedBNB.times(token0Amount))
-    .times(bundle.bnbPrice);
+    .plus(token0.derivedELA.times(token0Amount))
+    .times(bundle.elaPrice);
 
   // update txn counts
-  pancake.totalTransactions = pancake.totalTransactions.plus(ONE_BI);
+  glide.totalTransactions = glide.totalTransactions.plus(ONE_BI);
   pair.totalTransactions = pair.totalTransactions.plus(ONE_BI);
 
   // update global counter and save
   token0.save();
   token1.save();
   pair.save();
-  pancake.save();
+  glide.save();
 
   // update burn
   // burn.sender = event.params.sender
@@ -343,7 +343,7 @@ export function handleBurn(event: Burn): void {
 
   updatePairDayData(event);
   updatePairHourData(event);
-  updatePancakeDayData(event);
+  updateGlideDayData(event);
   updateTokenDayData(token0 as Token, event);
   updateTokenDayData(token1 as Token, event);
 }
@@ -361,21 +361,21 @@ export function handleSwap(event: Swap): void {
   let amount0Total = amount0Out.plus(amount0In);
   let amount1Total = amount1Out.plus(amount1In);
 
-  // BNB/USD prices
+  // ELA/USD prices
   let bundle = Bundle.load("1");
 
-  //log.info("hadleSwap - bundle.bnbPrice {}", [bundle.bnbPrice.toString()]);
-  //log.info("hadleSwap - token0.derivedBNB {}", [token0.derivedBNB.toString()]);
-  //log.info("hadleSwap - token1.derivedBNB {}", [token1.derivedBNB.toString()]);
+  //log.info("hadleSwap - bundle.elaPrice {}", [bundle.elaPrice.toString()]);
+  //log.info("hadleSwap - token0.derivedELA {}", [token0.derivedELA.toString()]);
+  //log.info("hadleSwap - token1.derivedELA {}", [token1.derivedELA.toString()]);
   //log.info("hadleSwap - amount0Total {}", [amount0Total.toString()]);
   //log.info("hadleSwap - amount1Total {}", [amount1Total.toString()]);
 
-  // get total amounts of derived USD and BNB for tracking
-  let derivedAmountBNB = token1.derivedBNB
+  // get total amounts of derived USD and ELA for tracking
+  let derivedAmountELA = token1.derivedELA
     .times(amount1Total)
-    .plus(token0.derivedBNB.times(amount0Total))
+    .plus(token0.derivedELA.times(amount0Total))
     .div(BigDecimal.fromString("2"));
-  let derivedAmountUSD = derivedAmountBNB.times(bundle.bnbPrice);
+  let derivedAmountUSD = derivedAmountELA.times(bundle.elaPrice);
   //log.info("hadleSwap - derivedAmountUSD {}", [derivedAmountUSD.toString()]);
   // only accounts for volume through white listed tokens
   let trackedAmountUSD = getTrackedVolumeUSD(
@@ -386,11 +386,11 @@ export function handleSwap(event: Swap): void {
     token1 as Token
   );
 
-  let trackedAmountBNB: BigDecimal;
-  if (bundle.bnbPrice.equals(ZERO_BD)) {
-    trackedAmountBNB = ZERO_BD;
+  let trackedAmountELA: BigDecimal;
+  if (bundle.elaPrice.equals(ZERO_BD)) {
+    trackedAmountELA = ZERO_BD;
   } else {
-    trackedAmountBNB = trackedAmountUSD.div(bundle.bnbPrice);
+    trackedAmountELA = trackedAmountUSD.div(bundle.elaPrice);
   }
 
   // update token0 global volume and token liquidity stats
@@ -416,17 +416,17 @@ export function handleSwap(event: Swap): void {
   pair.save();
 
   // update global values, only used tracked amounts for volume
-  let pancake = PancakeFactory.load(FACTORY_ADDRESS);
-  pancake.totalVolumeUSD = pancake.totalVolumeUSD.plus(trackedAmountUSD);
-  pancake.totalVolumeBNB = pancake.totalVolumeBNB.plus(trackedAmountBNB);
-  pancake.untrackedVolumeUSD = pancake.untrackedVolumeUSD.plus(derivedAmountUSD);
-  pancake.totalTransactions = pancake.totalTransactions.plus(ONE_BI);
+  let glide = GlideFactory.load(FACTORY_ADDRESS);
+  glide.totalVolumeUSD = glide.totalVolumeUSD.plus(trackedAmountUSD);
+  glide.totalVolumeELA = glide.totalVolumeELA.plus(trackedAmountELA);
+  glide.untrackedVolumeUSD = glide.untrackedVolumeUSD.plus(derivedAmountUSD);
+  glide.totalTransactions = glide.totalTransactions.plus(ONE_BI);
 
   // save entities
   pair.save();
   token0.save();
   token1.save();
-  pancake.save();
+  glide.save();
 
   let transaction = Transaction.load(event.transaction.hash.toHex());
   if (transaction === null) {
@@ -470,15 +470,15 @@ export function handleSwap(event: Swap): void {
   // update day entities
   let pairDayData = updatePairDayData(event);
   let pairHourData = updatePairHourData(event);
-  let pancakeDayData = updatePancakeDayData(event);
+  let glideDayData = updateGlideDayData(event);
   let token0DayData = updateTokenDayData(token0 as Token, event);
   let token1DayData = updateTokenDayData(token1 as Token, event);
 
   // swap specific updating
-  pancakeDayData.dailyVolumeUSD = pancakeDayData.dailyVolumeUSD.plus(trackedAmountUSD);
-  pancakeDayData.dailyVolumeBNB = pancakeDayData.dailyVolumeBNB.plus(trackedAmountBNB);
-  pancakeDayData.dailyVolumeUntracked = pancakeDayData.dailyVolumeUntracked.plus(derivedAmountUSD);
-  pancakeDayData.save();
+  glideDayData.dailyVolumeUSD = glideDayData.dailyVolumeUSD.plus(trackedAmountUSD);
+  glideDayData.dailyVolumeELA = glideDayData.dailyVolumeELA.plus(trackedAmountELA);
+  glideDayData.dailyVolumeUntracked = glideDayData.dailyVolumeUntracked.plus(derivedAmountUSD);
+  glideDayData.save();
 
   // swap specific updating for pair
   pairDayData.dailyVolumeToken0 = pairDayData.dailyVolumeToken0.plus(amount0Total);
@@ -494,17 +494,17 @@ export function handleSwap(event: Swap): void {
 
   // swap specific updating for token0
   token0DayData.dailyVolumeToken = token0DayData.dailyVolumeToken.plus(amount0Total);
-  token0DayData.dailyVolumeBNB = token0DayData.dailyVolumeBNB.plus(amount0Total.times(token0.derivedBNB as BigDecimal));
+  token0DayData.dailyVolumeELA = token0DayData.dailyVolumeELA.plus(amount0Total.times(token0.derivedELA as BigDecimal));
   token0DayData.dailyVolumeUSD = token0DayData.dailyVolumeUSD.plus(
-    amount0Total.times(token0.derivedBNB as BigDecimal).times(bundle.bnbPrice)
+    amount0Total.times(token0.derivedELA as BigDecimal).times(bundle.elaPrice)
   );
   token0DayData.save();
 
   // swap specific updating
   token1DayData.dailyVolumeToken = token1DayData.dailyVolumeToken.plus(amount1Total);
-  token1DayData.dailyVolumeBNB = token1DayData.dailyVolumeBNB.plus(amount1Total.times(token1.derivedBNB as BigDecimal));
+  token1DayData.dailyVolumeELA = token1DayData.dailyVolumeELA.plus(amount1Total.times(token1.derivedELA as BigDecimal));
   token1DayData.dailyVolumeUSD = token1DayData.dailyVolumeUSD.plus(
-    amount1Total.times(token1.derivedBNB as BigDecimal).times(bundle.bnbPrice)
+    amount1Total.times(token1.derivedELA as BigDecimal).times(bundle.elaPrice)
   );
   token1DayData.save();
 }
