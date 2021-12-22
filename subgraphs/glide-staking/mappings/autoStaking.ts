21b63fa15b529e6243b73542481e8fe1cc4a78b5
0xMirce <pegimira@yahoo.com>
2021-12-22 18:33:14 +0100

    Fix auto staking

--- subgraphs/glide-staking/mappings/autoStaking.ts
+++ subgraphs/glide-staking/mappings/autoStaking.ts
@@ -1,31 +1,38 @@
 import { AutoGlideStaking } from "../generated/schema";
-import { Deposit, Withdraw} from "../generated/GlideVault/GlideVault";
+import { Deposit, GlideVault, Withdraw} from "../generated/GlideVault/GlideVault";
 import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleDeposit(event: Deposit): void {
+    let glideVaultContract = GlideVault.bind(event.address);
+
     let entity = AutoGlideStaking.load(event.params.sender.toHex())
 
     if (!entity) {
         entity = new AutoGlideStaking(event.params.sender.toHex())
 
+        entity.shareAmount = BigInt.fromI32(0)
         entity.stakeAmount = BigInt.fromI32(0)
     }
 
-    entity.stakeAmount = entity.stakeAmount.plus(event.params.amount)
+    entity.shareAmount = entity.shareAmount.plus(event.params.shares)
+    entity.stakeAmount = entity.shareAmount.times(glideVaultContract.getPricePerFullShare()).div(BigInt.fromI32(10).pow(18))
 
     entity.save()
 }
   
 export function handleWithdraw(event: Withdraw): void {
+    let glideVaultContract = GlideVault.bind(event.address);
     let entity = AutoGlideStaking.load(event.params.sender.toHex())
 
     if (!entity) {
         entity = new AutoGlideStaking(event.params.sender.toHex())
 
+        entity.shareAmount = BigInt.fromI32(0)
         entity.stakeAmount = BigInt.fromI32(0)
     }
 
-    entity.stakeAmount = entity.stakeAmount.minus(event.params.amount)
+    entity.shareAmount = entity.shareAmount.minus(event.params.shares)
+    entity.stakeAmount = entity.shareAmount.times(glideVaultContract.getPricePerFullShare()).div(BigInt.fromI32(10).pow(18))
 
     entity.save()
 }
